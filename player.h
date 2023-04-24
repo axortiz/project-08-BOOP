cb0dfbb19a19361dc87d15236109e8dc1a9434b5
axortiz <123911710+axortiz@users.noreply.github.com>
2023-04-24 17:37:05 -0400

    Update player.h

--- player.h
+++ player.h
@@ -9,19 +9,3 @@ private:
 public:
     Player(string)
 };
-
-Player::Player(string given_name) {
-    cout << "Using the normal constructor for player" << endl;
-    name = given_name;
-}
-
-Player::Player(string given_name) 
-    :name(given_name)
-{
-    cout << "Using the MIL constructor for player" << endl;
-}
-
-int main() {
-    Player ("Cornelius");
-
-}
