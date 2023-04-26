592139307113f2a1829fd4550ddd005d144430f1
Papaya-sensei <99051293+Papaya-sensei@users.noreply.github.com>
2023-04-26 14:50:01 -0400

    Update - David 4/26

--- player.h
+++ player.h
@@ -1,11 +1,20 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 #include <iostream>
 #include <string>
 using namespace std;
 
 class Player {
 private:
-    Player() = delete;
     string name;
 public:
-    Player(string)
+    Player(string);
 };
+
+Player::Player(string given_name) 
+    :name(given_name)
+{
+    cout << "Using the MIL constructor for player" << endl;
+}
+
+#endif
