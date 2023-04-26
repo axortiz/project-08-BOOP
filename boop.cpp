ddbc3925aa5f1d723a21442786289c956d76d840
Papaya-sensei <99051293+Papaya-sensei@users.noreply.github.com>
2023-04-26 14:51:04 -0400

    Update - David 4/26

--- boop.cpp
+++ boop.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 
 int main() {
-    Boop game;
+    string name_1, name_2;
+    cout << "Player 1 name: " << endl;
+    cin >> name_1;
+    cout << "Player 2 name: " << endl;
+    cin >> name_2;
+    Boop game(name_1, name_2);
+
+    return 0;
 }
