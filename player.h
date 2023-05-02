8feabda1f33d2b3e5c33ace4fd7b50fb88bcefd2
Papaya-sensei <99051293+Papaya-sensei@users.noreply.github.com>
2023-05-02 19:33:26 -0400

    new

--- player.h
+++ player.h
@@ -2,19 +2,26 @@
 #define PLAYER_H
 #include <iostream>
 #include <string>
-using namespace std;
+#include "piece.h"
+#include <vector>
+#include <string>
+const int max_num = 8;
 
 class Player {
 private:
-    string name;
+    std::string name;
+    int num_pieces;
+protected:
+    std::vector <Piece*> my_pieces;
 public:
-    Player(string);
+    Player(std::string);
+    std::string get_name();
+    void graduate_kittens(int num_graduee);
+    Piece* retrieve_piece(bool get_cat);
+    Piece* retrieve_cat();
+    Piece* retrieve_kitten();
+    void display_pieces();
+    void receive_pieces(Piece* booped);
 };
-
-Player::Player(string given_name) 
-    :name(given_name)
-{
-    cout << "Using the MIL constructor for player" << endl;
-}
 
 #endif
