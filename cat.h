06fe0cf2728dbb0826c829d7acee5e06fb66455b
Papaya-sensei <99051293+Papaya-sensei@users.noreply.github.com>
2023-05-02 19:29:17 -0400

    new

--- cat.h
+++ cat.h
@@ -1,12 +1,18 @@
 #ifndef CAT_H
 #define CAT_H
 #include "piece.h"
-using namespace std;
+#include <string>
 
-class Cat : public piece {
-    private:
-    protected:
-    public:
+class Cat : public Piece {
+private:
+    std::string player_name;
+protected:
+public:
+    ~Cat();
+    Cat(std::string player_name);
+    bool is_cat();
+    std::string get_name();
+    void display();
 };
 
 #endif
