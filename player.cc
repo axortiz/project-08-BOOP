1dca0b4ea5b96dec05f8405e88bd0ec30f17c31b
Papaya-sensei <99051293+Papaya-sensei@users.noreply.github.com>
2023-05-02 19:33:03 -0400

    new

--- player.cc
+++ player.cc
@@ -1,5 +1,76 @@
+#include "player.h"
+#include "piece.h"
+#include "kitten.h"
+#include "cat.h"
+#include <vector>
+
+using namespace std;
+
 Player::Player(string given_name) 
-    :name(given_name)
+    :name(given_name),
+    num_pieces(8)
 {
-    cout << "Using the MIL constructor for player" << endl;
+    for (int i = 0; i < max_num; i++) {
+        my_pieces.push_back(new Kitten(name));
+    }
+}
+
+string Player::get_name(){
+    return name;
+}
+
+void Player::graduate_kittens(int num_graduee) {
+    for (int i = 0; i < num_graduee; i++) {
+        my_pieces.push_back(new Cat(name));
+        num_pieces++;
+    }
+}
+
+Piece* Player::retrieve_piece(bool get_cat) {
+    Piece* target_piece = nullptr;
+    if (get_cat) {
+        target_piece = retrieve_cat();
+        num_pieces--;
+    }
+    else {
+        target_piece = retrieve_kitten();
+        num_pieces--;
+    }
+    return target_piece;
+}
+
+Piece* Player::retrieve_cat() {
+    Piece* cat_piece = nullptr;
+    for (int i = 0; i < (int) my_pieces.size(); i++) {
+        if (my_pieces[i]->is_cat()) {
+            cat_piece = my_pieces[i];
+            my_pieces.erase(my_pieces.begin()+i);
+            break;
+        }
+    }
+    return cat_piece;
+}
+
+Piece* Player::retrieve_kitten() {
+    Piece* kitten_piece = nullptr;
+    for (int i = 0; i < (int) my_pieces.size(); i++) {
+        if (!my_pieces[i]->is_cat()) { 
+            kitten_piece = my_pieces[i];
+            my_pieces.erase(my_pieces.begin()+i);
+            break;
+        }
+    }
+    return kitten_piece;
+}
+
+void Player::display_pieces() {
+    for (int i = 0; i < (int) my_pieces.size(); i++) {
+        my_pieces[i]->display();
+    }
+    cout << endl;
+}
+
+void Player::receive_pieces(Piece* booped) {
+    my_pieces.push_back(booped);
+    num_pieces++;
 }
