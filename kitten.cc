3d79da8c018f12f2def71229e53bda01a8b35904
Papaya-sensei <99051293+Papaya-sensei@users.noreply.github.com>
2023-05-02 19:30:22 -0400

    new

---
+++ kitten.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "kitten.h"
+using namespace std;
+
+Kitten::~Kitten() {}
+
+Kitten::Kitten(string name) {
+     player_name = name;
+}
+
+bool Kitten::is_cat() {
+    return false;
+}
+
+string Kitten::get_name() {
+    return player_name;
+}
+
+void Kitten::display() {
+    cout << "Kitten "; // work on this after we have separate display
+}
