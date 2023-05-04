a8cbd53aabab9dad18491f597fa5d61746cdf3ba
axortiz <123911710+axortiz@users.noreply.github.com>
2023-05-03 22:10:42 -0400

    Almost done

--- game.h
+++ game.h
@@ -20,12 +20,15 @@ public:
     Player* get_player(Piece*);
     bool place_piece(Piece* cat_or_kitten, int x, int y);
     Piece* remove_button(int x_loc, int y_loc);
+    void delete_from_board(int x_loc, int y_loc);
     void check_coordinates_for_boop(int x, int y);
     void boop_piece(int x_loc, int y_loc, std::string path);
     void boop_cat(Piece* cat_or_kitten, int x_loc, int y_loc);
     bool check_boop_path(int x_loc, int y_loc);
     void deduct_cats(Player*);
     Piece* check_coordinates_for_three(int x, int y); 
+    void graduation();
+    void check_for_graduees(int x, int y);
     Piece* check_three_in_a_row_horizontal(int x, int y, std::string path);
     Piece* check_three_in_a_row_vertical(int x, int y, std::string path);
     Piece* check_three_in_a_row_diagonal(int x, int y, std::string path); 
